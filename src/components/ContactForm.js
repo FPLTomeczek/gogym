1f6da9a4ff52467bbc1a37c53c991cec40138e8f
FPLTomeczek <tomaszs18m@gmail.com>
2023-04-06 22:51:33 +0200

    contact_info

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,7 +2,28 @@ import React from "react";
 import "../css/ContactPage/ContactForm.css";
 
 const ContactForm = () => {
-  return <div>ContactForm</div>;
+  return (
+    <div className="contact-form-container">
+      <h2>Leave Us Your Info</h2>
+      <div className="underline"></div>
+      <form>
+        <input type="text" />
+        <input type="email" />
+        <select name="" id="">
+          <option>Body Building</option>
+          <option>Boxing</option>
+          <option>Running</option>
+          <option>Fitness</option>
+          <option>Yoga</option>
+          <option>Workout</option>
+          <option>Karate</option>
+          <option>Meditation</option>
+          <option>Cycling</option>
+        </select>
+        <button className="btn">submit now</button>
+      </form>
+    </div>
+  );
 };
 
 export default ContactForm;
--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,33 +1,41 @@
 import React from "react";
 import SocialLinks from "./SocialLinks";
+import "../css/ContactPage/ContactInfo.css";
 
 const ContactInfo = () => {
   return (
     <div className="contact-info-container">
       <h1>We are here for help you! To Shape Your Body.</h1>
-      <p>
+      <span>
         At Gymate, we are dedicated to helping you achieve the body of your
         dreams. Our expert trainers and nutritionists will work with you to
         create a personalized fitness and nutrition plan that helps you reach
         your specific goals.
-      </p>
+      </span>
       <div className="contact-infos">
         <div className="contact-info">
           <h3>New York City, USA</h3>
           <div className="underline"></div>
-          <span>85 Briston Mint Street, London, E1 8LG, USA</span>
+          <span>
+            85 Briston Mint Street,
+            <br /> London, E1 8LG, USA
+          </span>
         </div>
         <div className="contact-info">
           <h3>Opening Hours</h3>
           <div className="underline"></div>
           <span>
-            Mon to Fri: 7:30 am — 1:00 am Mon to Fri: 7:30 am — 1:00 am
+            Mon to Fri: 7:30 am — 1:00 am <br />
+            Mon to Fri: 7:30 am — 1:00 am
           </span>
         </div>
         <div className="contact-info">
           <h3>Information</h3>
           <div className="underline"></div>
-          <span>+800-123-4567 gymat@gymail.com</span>
+          <span>
+            +800-123-4567
+            <br /> gymat@gymail.com
+          </span>
         </div>
         <div className="contact-info">
           <h3>Follow Us On</h3>
